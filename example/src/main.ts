98d483bf7079462a4064115b3089faac552d6c1d
Gibson Murray <105567507+gibsonmurray@users.noreply.github.com>
2024-08-04 17:09:48 -0400

    started div renderer

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,2 +1,23 @@
-import render from "../../index"
-render();
+import render, { div } from "../../index"
+
+const newDiv = div(
+    ".test.test2",
+    {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "red",
+    },
+    {
+        onclick: () => {
+            console.log("clicked")
+        },
+        onmouseenter: () => {
+            console.log("hovered")
+        }
+    }
+)
+
+render(newDiv)
