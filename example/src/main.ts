5f0c94c25d8942eb0d1ac7dcf0a3da27a13df153
Gibson Murray <105567507+gibsonmurray@users.noreply.github.com>
2024-08-05 16:21:16 -0400

    render now takes an EtaElement parameter

--- example/src/main.ts
+++ example/src/main.ts
@@ -1,17 +1,1 @@
-import render from "../../index"
-import { div } from "../../elements"
 
-const newDiv = div(
-    {
-        width: "100px",
-        height: "100px",
-        backgroundColor: "red",
-    },
-    {
-        onclick: () => {
-            newDiv.moveTo(500, 500)
-        },
-    }
-)
-
-render(newDiv)
--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import "./eta-types"
+import type EtaElement from "./eta-types"
 
-export default function render(children: HTMLElement) {
+export default function render(children: EtaElement<HTMLElement>) {
     const root = document.querySelector("#root")
     if (root) {
         defaultStyles(document.body)
         root.innerHTML = ""
-        root.appendChild(children)
+        root.appendChild(children.element)
     } else {
         throw new Error(
             "Root element not found: please add a div with id='root' to your HTML file"
