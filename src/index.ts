6e8bca8784b5847a60d618f42cc6d54af73dbaa9
Gibson Murray <105567507+gibsonmurray@users.noreply.github.com>
2024-08-15 15:19:09 -0400

    1.2.2 fixes cdn bugs with router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Avita from "./avita"
 export * from "./elements"
 export { $ } from "./avita"
+export { default as AvitaRouter } from "./router"
 const render = Avita.render
 export { render }
 export default Avita
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,13 +3,11 @@ import { defineConfig } from "tsup"
 export default defineConfig({
     entry: {
         index: "src/index.ts",
-        router: "src/router.ts",
     },
-    format: ["esm", "cjs", "iife"],
+    format: ["esm", "iife"],
     globalName: "Avita",
     dts: true,
     clean: true,
-    sourcemap: true,
     skipNodeModulesBundle: true,
     target: "esnext",
     minify: true,
